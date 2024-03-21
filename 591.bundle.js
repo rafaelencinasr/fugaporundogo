21d78ae91a9dad2545d53fa3d892db1f9f48f94e
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2024-03-20 17:02:09 -0700

    Basic data creation form and database connection

---
+++ 591.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebpack_template=self.webpackChunkwebpack_template||[]).push([[591],{591:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
